d86638cf0e9239bfe70507bc359c9c5cc02f4e78
Manos Nathanail <m.nathanail@yahoo.gr>
2018-06-04 20:09:14 +0300

    Add project files.

---
+++ MovieModelAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovieModelAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
